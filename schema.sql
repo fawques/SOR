f67270179dd53bccf6d51270fa891890956fce5f
Víctor Guzmán <fawques@gmail.com>
2013-11-30 12:48:38 +0100

    añadidas fechas a pedido y oferta

--- schema.sql
+++ schema.sql
@@ -29,22 +29,25 @@ CREATE TABLE `taller` (
 
 CREATE TABLE `pedido` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` date NOT NULL,
+  `fecha_alta` date NOT NULL,
   `estado` int(11) NOT NULL,
-  `taller` int(11) DEFAULT NULL,
+  `taller` int(11) NOT NULL,
+  `fecha_baja` date DEFAULT NULL,
+  `fecha_limite` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `pedido_taller_FK_idx` (`taller`),
   CONSTRAINT `pedido_taller_FK` FOREIGN KEY (`taller`) REFERENCES `taller` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 CREATE TABLE `oferta` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` date NOT NULL,
+  `fecha_alta` date NOT NULL,
   `importe` double NOT NULL,
   `estado` int(11) NOT NULL,
-  `pedido` int(11) DEFAULT NULL,
-  `desguace` int(11) DEFAULT NULL,
+  `pedido` int(11) NOT NULL,
+  `desguace` int(11) NOT NULL,
+  `fecha_baja` date DEFAULT NULL,
+  `fecha_limite` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `oferta_pedido_FK_idx` (`pedido`),
   KEY `oferta_desguace_FK_idx` (`desguace`),
@@ -69,28 +72,30 @@ CREATE TABLE `taller` (
 
 CREATE TABLE `pedido` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` date NOT NULL,
+  `fecha_alta` date NOT NULL,
   `estado` int(11) NOT NULL,
-  `taller` int(11) DEFAULT NULL,
+  `taller` int(11) NOT NULL,
+  `fecha_baja` date DEFAULT NULL,
+  `fecha_limite` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `pedido_taller_FK_idx` (`taller`),
   CONSTRAINT `pedido_taller_FK` FOREIGN KEY (`taller`) REFERENCES `taller` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 CREATE TABLE `oferta` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` date NOT NULL,
+  `fecha_alta` date NOT NULL,
   `importe` double NOT NULL,
   `estado` int(11) NOT NULL,
-  `pedido` int(11) DEFAULT NULL,
-  `desguace` int(11) DEFAULT NULL,
+  `pedido` int(11) NOT NULL,
+  `desguace` int(11) NOT NULL,
+  `fecha_baja` date DEFAULT NULL,
+  `fecha_limite` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `oferta_pedido_FK_idx` (`pedido`),
   KEY `oferta_desguace_FK_idx` (`desguace`),
   CONSTRAINT `oferta_pedido_FK` FOREIGN KEY (`pedido`) REFERENCES `pedido` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 CREATE DATABASE IF NOT EXISTS `sor_desguace` /*!40100 DEFAULT CHARACTER SET utf8 */;
 use sor_desguace;
@@ -109,21 +114,24 @@ CREATE TABLE `desguace` (
 
 CREATE TABLE `pedido` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` date NOT NULL,
+  `fecha_alta` date NOT NULL,
   `estado` int(11) NOT NULL,
-  `taller` int(11) DEFAULT NULL,
+  `taller` int(11) NOT NULL,
+  `fecha_baja` date DEFAULT NULL,
+  `fecha_limite` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `pedido_taller_FK_idx` (`taller`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 CREATE TABLE `oferta` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` date NOT NULL,
+  `fecha_alta` date NOT NULL,
   `importe` double NOT NULL,
   `estado` int(11) NOT NULL,
-  `pedido` int(11) DEFAULT NULL,
-  `desguace` int(11) DEFAULT NULL,
+  `pedido` int(11) NOT NULL,
+  `desguace` int(11) NOT NULL,
+  `fecha_baja` date DEFAULT NULL,
+  `fecha_limite` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `oferta_pedido_FK_idx` (`pedido`),
   KEY `oferta_desguace_FK_idx` (`desguace`),
