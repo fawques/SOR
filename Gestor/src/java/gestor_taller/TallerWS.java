4accee79ce73e04f21acf52d7ff37f5712c3e928
Pablo Vicente Munuera <pablovm1990@gmail.com>
2013-11-30 14:47:23 +0100

    comenzando el alta del webservice

--- Gestor/src/java/gestor_taller/TallerWS.java
+++ Gestor/src/java/gestor_taller/TallerWS.java
@@ -6,7 +6,10 @@
 
 package gestor_taller;
 
+import BD.Conexion;
+import BD.InterfazBD;
 import general.Taller;
+import java.sql.SQLException;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -17,13 +20,18 @@ import javax.jws.WebParam;
  */
 @WebService(serviceName = "TallerWS")
 public class TallerWS {
-
+    
+    InterfazBD bd;
     /**
      * Web service operation
      */
     @WebMethod(operationName = "alta")
-    public Boolean alta(@WebParam(name = "ID") int ID,@WebParam(name = "name") String name, @WebParam(name = "email") String email, @WebParam(name = "address") String address, @WebParam(name = "city") String city, @WebParam(name = "postalCode") int postalCode, @WebParam(name = "telephone") int telephone) {
+    public Boolean alta(@WebParam(name = "ID") int ID,@WebParam(name = "name") String name, @WebParam(name = "email") String email, @WebParam(name = "address") String address, @WebParam(name = "city") String city, @WebParam(name = "postalCode") int postalCode, @WebParam(name = "telephone") int telephone) throws SQLException, ClassNotFoundException {
         Taller t = new Taller(ID,name, email, address, city, postalCode, telephone);
+        
+        bd = new InterfazBD("sor_taller");
+        
         return t != null?true:false;
+        //return codigoGestor
     }
 }
