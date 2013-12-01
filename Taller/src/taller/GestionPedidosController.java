049166925944dc8eb0d9fa72c150b35aaa1301b1
Pablo Vicente Munuera <pablovm1990@gmail.com>
2013-12-01 11:03:30 +0100

    gestion pedido taller, creado.. interfaz casi hecha

---
+++ Taller/src/taller/GestionPedidosController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package taller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.TimeZone;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author pablovm1990
+ */
+public class GestionPedidosController implements Initializable {
+
+    @FXML
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        TimeZone tz = null;
+    }    
+    
+}
