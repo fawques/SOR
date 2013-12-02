746f1fb2b133ea2843ca3f717c7dcda4f5f7da66
Pablo Vicente Munuera <pablovm1990@gmail.com>
2013-12-02 11:56:00 +0100

    all correct

--- Gestor/src/java/gestor_taller/TallerWS.java
+++ Gestor/src/java/gestor_taller/TallerWS.java
@@ -10,6 +10,8 @@ import BD.Conexion;
 import BD.InterfazBD;
 import general.Taller;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -35,9 +37,15 @@ public class TallerWS {
      * @throws ClassNotFoundException 
      */
     @WebMethod(operationName = "alta")
-    public int alta(@WebParam(name = "name") String name, @WebParam(name = "email") String email, @WebParam(name = "address") String address, @WebParam(name = "city") String city, @WebParam(name = "postalCode") int postalCode, @WebParam(name = "telephone") int telephone) throws SQLException, ClassNotFoundException {
-        
-        bd = new InterfazBD("sor_taller");
-        return bd.altaTaller(name, email, address, city, postalCode, telephone, 0);
+    public int alta(@WebParam(name = "name") String name, @WebParam(name = "email") String email, @WebParam(name = "address") String address, @WebParam(name = "city") String city, @WebParam(name = "postalCode") int postalCode, @WebParam(name = "telephone") int telephone) {
+        try {
+            bd = new InterfazBD("sor_gestor");
+            return bd.altaTaller(name, email, address, city, postalCode, telephone, 0);
+        } catch (SQLException ex) {
+            Logger.getLogger(TallerWS.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(TallerWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
     }
 }
