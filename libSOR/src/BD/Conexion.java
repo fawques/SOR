a1dc64fca91fcc10f5838a9d4dc23d2b16600e70
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-01-08 16:08:34 +0100

    capturada la excepcion para que siga ejecutando aunque al hacer insert hayas metido ya el mismo nombre de pieza

--- libSOR/src/BD/Conexion.java
+++ libSOR/src/BD/Conexion.java
@@ -112,19 +112,23 @@ public class Conexion {
         int numero=-1;
         try {
             try (Statement stmt = conexion.createStatement()) {
-                stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
-                ResultSet rs = stmt.getGeneratedKeys();
-                if (rs.next()){
-                    numero=rs.getInt(1);
-                }else{
-                    numero = 0;
+                try {
+                    stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+                    ResultSet rs = stmt.getGeneratedKeys();
+                    if (rs.next()) {
+                        numero = rs.getInt(1);
+                    } else {
+                        numero = 0;
+                    }
+                    rs.close();
+                    return numero;
+                } catch (com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException ex) {
+                    System.err.println(ex.getMessage());
                 }
-                rs.close();
-                return numero;
-            }
+            } 
         } catch (SQLException ex) {
             Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        } 
         return numero;
     }
     
