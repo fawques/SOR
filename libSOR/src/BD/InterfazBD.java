5fd97256703b02000467a9b09e7d66095b62d38f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-01-09 13:48:45 +0100

    anyadido modificarTaller sobrecargado

--- libSOR/src/BD/InterfazBD.java
+++ libSOR/src/BD/InterfazBD.java
@@ -420,6 +420,10 @@ public class InterfazBD {
     public boolean modificarTaller(String ID, String nombre, String email, String direccion, String ciudad, int codPostal, int telefono, EstadoGeneral estado){
         return conexion.ejecutarSQL("UPDATE `taller` SET `nombre`='" + nombre + "', `email`='" + email + "', `direccion`='" + direccion + "', `ciudad`='" + ciudad + "', `codPostal`='" + codPostal + "', `telefono`='" + telefono + "', `estado`='" + estado.ordinal() + "' WHERE `id`='" + ID + "';");
     }
+
+    public boolean modificarTaller(String nombre, String email, String direccion, String ciudad, int codPostal, int telefono) {
+        return conexion.ejecutarSQL("UPDATE `taller` SET `nombre`='" + nombre + "', `email`='" + email + "', `direccion`='" + direccion + "', `ciudad`='" + ciudad + "', `codPostal`='" + codPostal + "', `telefono`='" + telefono + "'");
+    }
     
     public void close(){
         conexion.closeConexion();
