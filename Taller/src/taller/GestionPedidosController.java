19613c0dbd51b7becaa0f7780bbf2900bade7036
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-01-09 13:51:17 +0100

    anyadidas funciones en el controlador de modificar (altatallercontroller)

--- Taller/src/taller/GestionPedidosController.java
+++ Taller/src/taller/GestionPedidosController.java
@@ -406,6 +406,10 @@ public class GestionPedidosController implements Initializable {
         }
     }
 
+    /**
+     *
+     * @throws IOException
+     */
     public void bajaTaller() throws IOException {
         if (MainTaller.bajaTaller()) {
             cambiarAPantallaTallerDeBaja();
@@ -426,7 +430,18 @@ public class GestionPedidosController implements Initializable {
         tdCont.showStage();
     }
 
-    public void modificarTaller() {
-
+    /**
+     *
+     */
+    public void modificarTaller() throws IOException {
+        URL location = getClass().getResource("modificarDatos.fxml");
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(location);
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+        Parent page = (Parent) loader.load(location.openStream());
+        thisStage.getScene().setRoot(page);
+        AltaTallerController tdCont = (AltaTallerController) loader.getController();
+        tdCont.setStage(thisStage);
+        tdCont.showStage();
     }
 }
