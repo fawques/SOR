4e786642e5d3a9ff09f646cd1f055c8610d15017
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-01-09 11:47:50 +0100

    reactivar taller completo

--- Taller/src/taller/TallerDeBajaController.java
+++ Taller/src/taller/TallerDeBajaController.java
@@ -6,9 +6,13 @@
 
 package taller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 
 /**
@@ -28,8 +32,18 @@ public class TallerDeBajaController implements Initializable {
         // TODO
     }
 
-    public void reactivarTaller() {
-        MainTaller.reactivarTaller();
+    public void reactivarTaller() throws IOException {
+        if (MainTaller.reactivarTaller()) {
+            URL location = getClass().getResource("GestionPedidos.fxml");
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(location);
+            loader.setBuilderFactory(new JavaFXBuilderFactory());
+            Parent page = (Parent) loader.load(location.openStream());
+            thisStage.getScene().setRoot(page);
+            GestionPedidosController tdCont = (GestionPedidosController) loader.getController();
+            tdCont.setStage(thisStage);
+            tdCont.showStage();
+        }
     }
 
     /**
