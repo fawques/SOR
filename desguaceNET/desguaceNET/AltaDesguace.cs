13fa30f4176979f078e6fee68368a7f60fc27459
Víctor Guzmán <fawques@gmail.com>
2014-01-19 16:11:12 +0100

    Creado el proyecto en VS2013

---
+++ desguaceNET/desguaceNET/AltaDesguace.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desguaceNET
+{
+    public partial class AltaDesguace : Form
+    {
+        public AltaDesguace()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sor_desguaceDataSet dataSet = new sor_desguaceDataSet();
+            sor_desguaceDataSetTableAdapters.desguaceTableAdapter aux = new sor_desguaceDataSetTableAdapters.desguaceTableAdapter();
+            aux.Fill(dataSet.desguace);
+            System.Data.DataRow[] filas = dataSet.desguace.Select();
+            foreach (var item in filas)
+            {
+                Console.WriteLine("AAA");
+                Console.WriteLine(item["email"]);
+                Console.WriteLine("AAA");
+            }
+        }
+    }
+}
---
+++ desguaceNET/desguaceNET/NuevaOferta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desguaceNET
+{
+    public partial class NuevaOferta : Form
+    {
+        public NuevaOferta()
+        {
+            InitializeComponent();
+        }
+    }
+}
