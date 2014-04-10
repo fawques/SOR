e0d43d176044b59db003736eb8ad955ac5ae26fa
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-04-10 20:13:57 +0200

    a prori autenticacion en Desguace

--- Gestor/src/gestor_desguace_java/DesguaceJavaWS.java
+++ Gestor/src/gestor_desguace_java/DesguaceJavaWS.java
@@ -20,9 +20,11 @@ import general.EstadoPedido;
 import general.Oferta;
 import general.Pedido;
 import general.PedidoCorto;
+import general.Taller;
 import gestor_taller.TallerWS;
 
 import java.lang.reflect.Type;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -30,12 +32,16 @@ import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.crypto.SecretKey;
 import javax.jms.JMSException;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
+
+import seguridad.TripleDes;
 
 /**
  *
@@ -44,6 +50,90 @@ import org.apache.commons.codec.digest.DigestUtils;
 @WebService(serviceName = "DesguaceJavaWS")
 public class DesguaceJavaWS {
     InterfazBD bd;
+    
+
+	static ArrayList<String> listaIdDesguace;
+	static ArrayList<SecretKey> listaSecretKeys;
+    
+	private SecretKey getKey(String idDesguace, String password) {
+		int index = listaIdDesguace.indexOf(idDesguace);
+		if (index != -1) {
+			bd=new InterfazBD("sor_gestor");
+			Desguace desg = bd.getDesguaceEnGestor(idDesguace);
+			if (desg != null && desg.getPassword().equals(password)) {
+				SecretKey key = listaSecretKeys.get(index);
+				return key;
+			}
+		} 
+		return null;
+	}
+	
+	static public void removeKey(String idTaller) {
+		int index = listaIdDesguace.indexOf(idTaller);
+		if (index != -1) {
+			listaSecretKeys.remove(index);
+			listaIdDesguace.remove(index);
+		} else {
+			System.err.println("ERROR - idTaller no esta en la lista de claves de Reto");
+		}
+	}
+	
+    public String generarClaveReto(@WebParam(name = "idDesguace") String idDesguace,@WebParam(name = "password") String password) {
+		// Generamos la clave de reto y se la mandamos al cliente
+		try {
+			bd=new InterfazBD("sor_gestor");
+			Desguace desg = bd.getDesguaceEnGestor(idDesguace);
+			if (desg != null && desg.getPassword().equals(password)) {
+				try {
+					SecretKey clave = TripleDes.generateKey();
+
+					if (listaIdDesguace.indexOf(idDesguace) != -1) {
+						// borramos el anterior
+						listaSecretKeys.remove(listaIdDesguace.indexOf(idDesguace));
+						listaIdDesguace.remove(listaIdDesguace.indexOf(idDesguace));
+					}
+					// anyadir nuevo
+					listaIdDesguace.add(idDesguace);
+					listaSecretKeys.add(clave);
+					Base64 b64 = new Base64();
+					bd.close();
+					return b64.encodeToString(clave.getEncoded());
+				} catch (NoSuchAlgorithmException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				System.err
+						.println("id taller incorrecto. Clave de reto no generada");
+			}
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		bd.close();
+		return null;
+	}
+    
+    private boolean comprobarPass(String idDesguace, String pass){
+		try {
+			bd = new InterfazBD("sor_gestor");
+			Taller t = bd.getDesguaceEnGestor(idDesguace);
+			bd.close();
+			if(t!=null){
+				if(t.getPassword().equals(pass))
+					return true;
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+    
       @WebMethod(operationName = "alta")
     public boolean alta(@WebParam(name = "name") String name, @WebParam(name = "email") String email, @WebParam(name = "address") String address, @WebParam(name = "city") String city, @WebParam(name = "postalCode") String postalCode, @WebParam(name = "telephone") String telephone) {
     
@@ -101,18 +191,23 @@ public class DesguaceJavaWS {
      * Web service operation
      */
     @WebMethod(operationName = "nuevaOferta")
-    public String nuevaOferta(@WebParam(name = "oferta") String oferta) {
+    public String nuevaOferta(@WebParam(name = "oferta") String oferta, @WebParam(name = "idDesguace") String idDesguace, @WebParam(name = "password") String password) {
         try {          
-            bd = new InterfazBD("sor_gestor");
              Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create();
-
-             Oferta p = gson.fromJson(oferta,Oferta.class);
+             SecretKey key = getKey(idDesguace, password)		;
+ 			if (key != null) {
+             Oferta p = gson.fromJson(TripleDes.decrypt(key, oferta),Oferta.class);
             Date ahora = new Date();
             String stringID  = "Oferta_" + bd.getNumOfertas();
             p.setID(stringID);
+            bd = new InterfazBD("sor_gestor");
              bd.anadirOferta(stringID,p.getID_aux(), p.getFecha_alta(),p.getPrecio(), EstadoOferta.ACTIVE.ordinal(),  p.getPedido(), p.getDesguace(),p.getDesguaceNombre(), p.getFecha_baja(),p.getFecha_limite());
-            bd.close();
-            return stringID;
+             bd.close();
+             return stringID;
+ 			}
+ 			else{
+				System.err.println("secretKey = NULL!");
+ 			}
         } catch (SQLException ex) {
             Logger.getLogger(DesguaceJavaWS.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
@@ -198,22 +293,28 @@ public class DesguaceJavaWS {
      * Web service operation
      */
     @WebMethod(operationName = "aceptarOfertaFin")
-    public Boolean aceptarOfertaFin(@WebParam(name = "id") String id) {
+    public Boolean aceptarOfertaFin(@WebParam(name = "id") String id, @WebParam(name = "idDesguace") String idDesguace, @WebParam(name = "password") String password) {
         Boolean aceptada=false;
         try {
-            bd = new InterfazBD("sor_gestor");
-            Oferta of= bd.getOfertaporID(id);
-            aceptada=bd.cambiarEstadoOferta(EstadoOferta.FINISHED_OK, id);
-            Pedido ped= bd.getPedido(of.getPedido());
-            ArrayList<Oferta> listaoferta=bd.getOfertasPedido(ped.getID());
-            for(Oferta oferta:listaoferta){
-            	if(oferta.getEstado()==EstadoOferta.ACTIVE ||oferta.getEstado()==EstadoOferta.NEW ||oferta.getEstado()==EstadoOferta.ACCEPTED){
-            		bd.cambiarEstadoOferta(EstadoOferta.REJECTED, oferta.getID());
-            	}
-            }
-            bd.cambiarEstadoPedido(EstadoPedido.FINISHED_OK,ped.getID());
-            bd.close();
-            return aceptada;
+        	SecretKey key = getKey(idDesguace, password)		;
+ 			if (key != null) {
+	            bd = new InterfazBD("sor_gestor");
+	            Oferta of= bd.getOfertaporID(TripleDes.decrypt(key, id));
+	            aceptada=bd.cambiarEstadoOferta(EstadoOferta.FINISHED_OK, id);
+	            Pedido ped= bd.getPedido(of.getPedido());
+	            ArrayList<Oferta> listaoferta=bd.getOfertasPedido(ped.getID());
+	            for(Oferta oferta:listaoferta){
+	            	if(oferta.getEstado()==EstadoOferta.ACTIVE ||oferta.getEstado()==EstadoOferta.NEW ||oferta.getEstado()==EstadoOferta.ACCEPTED){
+	            		bd.cambiarEstadoOferta(EstadoOferta.REJECTED, oferta.getID());
+	            	}
+	            }
+	            bd.cambiarEstadoPedido(EstadoPedido.FINISHED_OK,ped.getID());
+	            bd.close();
+	            return aceptada;
+ 			}
+ 			else{
+ 				
+ 			}
             
         } catch (SQLException ex) {
             Logger.getLogger(DesguaceJavaWS.class.getName()).log(Level.SEVERE, null, ex);
@@ -227,14 +328,20 @@ public class DesguaceJavaWS {
      * Web service operation
      */
     @WebMethod(operationName = "cancelarOferta")
-    public Boolean cancelarOferta(@WebParam(name = "id") String id) {
+    public Boolean cancelarOferta(@WebParam(name = "id") String id, @WebParam(name = "idDesguace") String idDesguace, @WebParam(name = "password") String password) {
         Boolean aceptada=false;
         try {
-            bd = new InterfazBD("sor_gestor");
-            aceptada=bd.cambiarEstadoOferta(EstadoOferta.CANCELLED, id);
-            bd.close();
-            
-            return aceptada;
+        	SecretKey key = getKey(idDesguace, password);
+ 			if (key != null) {
+	            bd = new InterfazBD("sor_gestor");
+	            aceptada=bd.cambiarEstadoOferta(EstadoOferta.CANCELLED, id);
+	            bd.close();
+	            
+	            return aceptada;
+ 			}
+ 			else{
+ 				
+ 			}
             
         } catch (SQLException ex) {
             Logger.getLogger(DesguaceJavaWS.class.getName()).log(Level.SEVERE, null, ex);
@@ -268,16 +375,21 @@ public class DesguaceJavaWS {
      * Web service operation
      */
     @WebMethod(operationName = "cambiarEstadoPedido")
-    public Boolean cambiarEstadoPedido(@WebParam(name = "id") String id, @WebParam(name = "estado") String estado) {
+    public Boolean cambiarEstadoPedido(@WebParam(name = "id") String id, @WebParam(name = "estado") String estado, @WebParam(name = "idDesguace") String idDesguace, @WebParam(name = "password") String password) {
          
            
         try {
-            bd= new InterfazBD("sor_gestor");
-            System.out.println("miau");
-           Boolean ool = bd.cambiarEstadoPedido(EstadoPedido.valueOf(estado), id);
-           bd.close();
-           return ool;
-      
+        	SecretKey key = getKey(idDesguace, password)		;
+ 			if (key != null) {
+	            bd= new InterfazBD("sor_gestor");
+	            System.out.println("miau");
+	           Boolean ool = bd.cambiarEstadoPedido(EstadoPedido.valueOf(estado), id);
+	           bd.close();
+	           return ool;
+ 			}
+ 			else{
+ 				
+ 			}
         } catch (SQLException ex) {
             Logger.getLogger(DesguaceJavaWS.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
@@ -288,14 +400,20 @@ public class DesguaceJavaWS {
         return false;
     }
     @WebMethod(operationName = "cambiarEstadoPedidoOtravez")
-    public Boolean cambiarEstadoPedidoOtravez(@WebParam(name = "id") String id, @WebParam(name = "estado") String estado) {
+    public Boolean cambiarEstadoPedidoOtravez(@WebParam(name = "id") String id, @WebParam(name = "estado") String estado, @WebParam(name = "idDesguace") String idDesguace, @WebParam(name = "password") String password) {
          
            
         try {
-            bd= new InterfazBD("sor_gestor");
-           Boolean ool = bd.cambiarEstadoPedido(EstadoPedido.valueOf(estado), id);
-           bd.close();
-           return ool;
+        	SecretKey key = getKey(idDesguace, password)		;
+ 			if (key != null) {
+	            bd= new InterfazBD("sor_gestor");
+	           Boolean ool = bd.cambiarEstadoPedido(EstadoPedido.valueOf(estado), id);
+	           bd.close();
+	           return ool;
+ 			}
+ 			else{
+ 				
+ 			}
       
         } catch (SQLException ex) {
             Logger.getLogger(DesguaceJavaWS.class.getName()).log(Level.SEVERE, null, ex);
@@ -306,12 +424,26 @@ public class DesguaceJavaWS {
        
         return false;
     }
-    public boolean modificar(@WebParam(name = "id") String id, @WebParam(name = "name") String name, @WebParam(name = "email") String email, @WebParam(name = "address") String address, @WebParam(name = "city") String city, @WebParam(name = "postalCode") String postalCode, @WebParam(name = "telephone") String telephone) {
+    public boolean modificar(@WebParam(name = "id") String id, @WebParam(name = "name") String name, @WebParam(name = "email") String email, @WebParam(name = "address") String address, @WebParam(name = "city") String city, @WebParam(name = "postalCode") String postalCode, @WebParam(name = "telephone") String telephone, @WebParam(name = "password") String password) {
         try {
-            bd = new InterfazBD("sor_gestor");
-            boolean res = bd.modificarDesguace(id, name, email, city, city, Integer.parseInt(postalCode), Integer.parseInt(telephone), EstadoGeneral.ACTIVE);
-            bd.close();
-            return res;
+			SecretKey key = getKey(id, password);
+			if (key != null) {
+	            bd = new InterfazBD("sor_gestor");
+				// desencriptar elementos
+				boolean res = bd.modificarTaller(id,
+						TripleDes.decrypt(key, name),
+						TripleDes.decrypt(key, email),
+						TripleDes.decrypt(key, city),
+						TripleDes.decrypt(key, city),
+						Integer.parseInt(TripleDes.decrypt(key, postalCode)),
+						Integer.parseInt(TripleDes.decrypt(key, telephone)),
+						EstadoGeneral.ACTIVE);
+				bd.close();
+				removeKey(id);
+	            return res;
+			} else {
+				System.err.println("secretKey = NULL!");
+			}
         } catch (java.sql.SQLException ex) {
             Logger.getLogger(TallerWS.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
