99af0d1e090c241e118437a32d4c4e78d25d2910
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-05-05 14:23:44 +0200

    metodo para cambiar de scene

--- AltaDisponibilidad/src/altaDisponibilidad/AltaDisponibilidad.java
+++ AltaDisponibilidad/src/altaDisponibilidad/AltaDisponibilidad.java
@@ -3,7 +3,14 @@
  */
 package altaDisponibilidad;
 
+import java.io.IOException;
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -11,11 +18,30 @@ import javafx.stage.Stage;
  *
  */
 public class AltaDisponibilidad extends Application {
+	
+	public static Stage stage;
 
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage stage2) throws Exception {
 		// TODO Auto-generated method stub
-		
+		stage = stage2;
 	}
 
+	public static FXMLLoader changeScene(String fxml) throws IOException {
+		URL location = AltaDisponibilidad.class.getResource(fxml);
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(location);
+		loader.setBuilderFactory(new JavaFXBuilderFactory());
+		Parent page = (Parent) loader.load(location.openStream());
+		if (stage.getScene() == null) {
+			Scene scene = new Scene(page);
+			stage.setScene(scene);
+		} else {
+			stage.getScene().setRoot(page);
+			stage.sizeToScene();
+		}
+
+		return loader;
+
+	}
 }
