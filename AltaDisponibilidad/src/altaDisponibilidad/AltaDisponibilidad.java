f7eb34eee9242bac4b53c3a312c605a7d431a45d
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-05-05 14:20:00 +0200

    inicio de proyecto

---
+++ AltaDisponibilidad/src/altaDisponibilidad/AltaDisponibilidad.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package altaDisponibilidad;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author pablovm1990
+ *
+ */
+public class AltaDisponibilidad extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
