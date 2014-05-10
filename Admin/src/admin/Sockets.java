2d1abb7e3a4f47a260d7875d4735e73c2062e49e
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-05-10 17:04:03 +0200

    clase sockets

---
+++ Admin/src/admin/Sockets.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package admin;
+
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.net.Socket;
+
+/**
+ * @author pablovm1990
+ *
+ */
+public class Sockets {
+	static final String HOST = "localhost"; 
+	static final int PUERTO=5000;
+	
+	public Sockets() {
+		try {
+			Socket skCliente = new Socket(HOST, PUERTO);
+			InputStream aux = skCliente.getInputStream();
+			DataInputStream flujo = new DataInputStream(aux);
+			System.out.println(flujo.readUTF());
+			skCliente.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
