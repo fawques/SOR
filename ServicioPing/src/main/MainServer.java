70d89c7b03bd4e8ec86ebe116154fc507a081d53
Víctor Guzmán <fawques@gmail.com>
2014-05-15 19:26:20 +0200

    Arreglados algunos fallos del servicioPing. Ahora levanta el socket de nuevo cuando da un socketException y loguea cosas al visor de eventos

--- ServicioPing/src/main/MainServer.java
+++ ServicioPing/src/main/MainServer.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 import org.boris.winrun4j.AbstractService;
 import org.boris.winrun4j.ServiceException;
+import org.boris.winrun4j.EventLog;
 
 import wschecker.WSChecker;
 
@@ -20,7 +22,11 @@ public class MainServer extends AbstractService {
 	
 	@Override
 	public int serviceMain(String[] args) throws ServiceException {
-		start();
+		try{
+			start();
+		}catch(Exception e){
+			EventLog.report("WSPingSOR", EventLog.ERROR, "ERROR Exception general <" + e.getMessage() + ">");
+		}
 		return 0;
 	}
 	
@@ -31,20 +37,33 @@ public class MainServer extends AbstractService {
 		try {
 			skServidor = new ServerSocket(PUERTO);
 			while(!shutdown){
-				Socket skCliente = skServidor.accept();
-				OutputStream aux = skCliente.getOutputStream();
-				DataOutputStream flujo= new DataOutputStream(aux);
-				
-				if(checker.ping()){
-					flujo.writeUTF("OK");
-				}else{
-					flujo.writeUTF("ERROR");
+				Socket skCliente = null;
+				try{
+					System.out.println("Aceptando conexiones en el puerto " + PUERTO);
+					EventLog.report("WSPingSOR", EventLog.INFORMATION, "Aceptando conexiones en el puerto " + PUERTO);
+					skCliente = skServidor.accept();
+					OutputStream aux = skCliente.getOutputStream();
+					DataOutputStream flujo= new DataOutputStream(aux);
+					
+					if(checker.ping()){
+						flujo.writeUTF("OK");
+					}else{
+						flujo.writeUTF("ERROR");
+					}
+					
+					skCliente.close();
+				}catch(SocketException ex){
+					skCliente.close();
+					skServidor.close();
+					System.err.println("Reiniciando el server");
+	                EventLog.report("WSPingSOR", EventLog.ERROR, "Reiniciando el servidor <" + ex.getMessage() + ">");
+					ex.printStackTrace();
+					skServidor = new ServerSocket(PUERTO);
 				}
 				
-				skCliente.close();
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+            EventLog.report("WSPingSOR", EventLog.ERROR, "ERROR IOException <" + e.getMessage() + ">");
 			e.printStackTrace();
 		}		
 	}
